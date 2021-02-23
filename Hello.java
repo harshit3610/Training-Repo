e3cbd8e628875269004e1d3c4a604863f9681a4a
harshit3610 <46259317+harshit3610@users.noreply.github.com>
2021-02-23 10:52:49 +0530

    Added a Lambda

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
+import java.util.Function
 class Hello{
     public static void main(String args[]) {
         System.out.println("Hello,World!");
+        Function<Integer,Integer> f1=(x)->x*10;
     }    
 }
 

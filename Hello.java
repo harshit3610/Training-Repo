7377e66976e009887e0a2ada244cc3b528fa0ce3
harshit3610 <46259317+harshit3610@users.noreply.github.com>
2021-02-23 11:02:29 +0530

    one new line added

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Hello{
     public static void main(String args[]) {
         System.out.println("Hello,World!");
         Function<Integer,Integer> f1=(x)->x*10;
+        System.out.println("DONE");
     }    
 }
 
